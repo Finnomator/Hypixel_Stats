8b8811f3fc79e4e92339fe3320e938331e687951
Finnomator <finndruenert@gmail.com>
2022-02-17 16:43:46 +0100

    removed threading

--- src/main.py
+++ src/main.py
@@ -1,7 +1,6 @@
 from datetime import datetime
 import json
 import sys
-from threading import Thread
 import os
 from tqdm import tqdm
 import detect_who
@@ -17,8 +16,6 @@ with open("info.json", "r") as f:
 api_key = infos["api_key"]
 mc_version = infos["mc_version"]
 mc_client = infos["client"]
-
-corrupted_info = False
 
 detector = detect_who.main(mc_client, mc_version)
 
@@ -36,60 +33,29 @@ def log_exception(exception: str):
 
 def update_infos(data: dict):
 
-    try:
-        with open("info.json", "r") as f:
-            old = json.load(f)
-    except json.decoder.JSONDecodeError:
+    global infos
 
-        global corrupted_info
-
-        corrupted_info = True
-
-        print()
-        print("ERROR:")
-        print("info.json got corrupted")
-        print("This is most likely because multiple instances of this program were running")
-        print("Please close all instances and restart the program")
-
-        for i in range(30)[::-1]:
-            print("This instance will close itself in " +
-                  str(i)+" seconds! ", end="\r")
-
-            time.sleep(1)
-
-        os._exit(1)
-
-    old.update(data)
+    infos.update(data)
 
     with open("info.json", "w") as f:
-        json.dump(old, f, indent=4)
-
-    return old
+        json.dump(infos, f, indent=4)
 
 
-def reset_left_requests(stop):
+def reset_left_requests():
 
-    while not stop():
-        min_now = datetime.now().minute
+    min_now = datetime.now().minute
 
-        if min_now != infos["request"]["last_request"]:
-            update_infos(
-                {"request": {"left_requests": 120, "last_request": min_now}})
-
-        time.sleep(5)
-
-
-stop_thread = False
-t1 = Thread(target=reset_left_requests, args=(lambda: stop_thread, ))
-t1.start()
+    if min_now != infos["request"]["last_request"]:
+        update_infos(
+            {"request": {"left_requests": 120, "last_request": min_now}})
 
 print()
-print("#"*100)
-print("IMPORTANT: Always make sure that only one instance of this program is running!!!")
-print("Even if an exception got raised the program will remain running.")
-print("To close an instance just close the terminal.")
-print("#"*100)
+print("#"*51)
+print("# Please make sure you only have one instance of  #\n# this program runnig or else you will waste your #\n# api requests (which are limited to 120/min)     #")
+print("#"*51)
 print()
+
+reset_left_requests()
 
 while True:
 
@@ -98,21 +64,19 @@ while True:
     try:
         for detect in detector:
 
-            if corrupted_info:
-                continue
-
             os.system("cls")
 
             print("Searching...")
 
             players_data = {}
 
+            reset_left_requests()
+
             left_requests = infos["request"]["left_requests"]
 
             for player in tqdm(detect):
 
                 if left_requests == 0:
-                    print("Out of requests, please wait 60s")
                     break
 
                 processed_player_data = process_data.process_player_data(
@@ -121,12 +85,16 @@ while True:
 
                 left_requests -= 1
 
+            update_infos(
+                {"request": {"left_requests": left_requests, "last_request": datetime.now().minute}})
+
+            if left_requests == 0:
+                print("Out of requests, please wait 60s")
+                continue
+
             extracted_player_data = process_data.extract(players_data)
 
             pretty_res = process_data.pretty_format(extracted_player_data)
-
-            infos = update_infos(
-                {"request": {"left_requests": left_requests, "last_request": datetime.now().minute}})
 
             print(pretty_res)
 
@@ -134,14 +102,12 @@ while True:
 
     except KeyboardInterrupt:
         print("Stopping...")
-        stop_thread = True
-        t1.join()
         exit()
 
     except Exception as e:
 
         e_msg = ""
-        unwichtig, exc_obj, exc_tb = sys.exc_info()
+        _, exc_obj, exc_tb = sys.exc_info()
         fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
 
         e_msg += str(fname)
@@ -149,6 +115,6 @@ while True:
         e_msg += str(e)
 
         print("An exception occurred: " + str(e))
-        print("See src/log.json for more information")
+        print("Check src/log.json for more information")
         print("If this error occurres again please report it on https://github.com/Finnomator/Hypixel_Stats/issues")
         log_exception(e_msg)
